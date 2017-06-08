467d4fc3c3c5e2c343200ba77ea002eadfde920b
zhehui <zhehui.zhou@outlook.com>
2017-06-08 00:14:43 -0400

    minor

--- app/src/main/java/com/example/zhehui/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/zhehui/inventoryapp/EditorActivity.java
@@ -80,18 +80,18 @@ public class EditorActivity extends AppCompatActivity implements
             setTitle(getString(R.string.editor_activity_title_new_item));
 
             // Invalidate the options menu, so the "Delete" menu option can be hidden.
-            // (It doesn't make sense to delete a item that hasn't been created yet.)
+            // It doesn't make sense to delete a item that hasn't been created yet.
             invalidateOptionsMenu();
         } else {
-            // Otherwise this is an existing item, so change app bar to say "Edit Item"
+            // Otherwise this is an existing item, so change app bar to say "Edit Item".
             setTitle(getString(R.string.editor_activity_title_edit_item));
 
             // Initialize a loader to read the item data from the database
-            // and display the current values in the editor
+            // and display the current values in the editor.
             getLoaderManager().initLoader(EXISTING_ITEM_LOADER, null, this);
         }
 
-        // Find all relevant views that we will need to read user input from
+        // Find all relevant views that we will need to read user input from.
         mNameEditText = (EditText) findViewById(R.id.edit_item_name);
         mPriceEditText = (EditText) findViewById(R.id.edit_item_price);
         mQuantityEditText = (EditText) findViewById(R.id.edit_item_quantity);
@@ -125,15 +125,14 @@ public class EditorActivity extends AppCompatActivity implements
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        // Bail early if the cursor is null or there is less than 1 row in the cursor.
         if (cursor == null || cursor.getCount() < 1) {
             return;
         }
 
-        // Proceed with moving to the first row of the cursor and reading data from it
-        // (This should be the only row in the cursor)
+        // Proceed with moving to the first row of the cursor and reading data from it.
+        // This should be the only row in the cursor.
         if (cursor.moveToFirst()) {
-
             // Find the columns of item attributes that we're interested in.
             int nameColumnIndex = cursor.getColumnIndex(
                     InventoryItemContract.InventoryItemEntry.COLUMN_ITEM_NAME);
@@ -147,7 +146,7 @@ public class EditorActivity extends AppCompatActivity implements
             Float price = cursor.getFloat(priceColumnIndex);
             Integer quantity = cursor.getInt(quantityColumnIndex);
 
-            // Update the views on the screen with the values from the database
+            // Update the views on the screen with the values from the database.
             mNameEditText.setText(name);
             mPriceEditText.setText(Float.toString(price));
             mQuantityEditText.setText(Integer.toString(quantity));
