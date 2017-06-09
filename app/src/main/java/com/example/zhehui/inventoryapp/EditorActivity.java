afef666a7ef90a5b4903a5ce5f72f4fabb136d75
zhehui <zhehui.zhou@outlook.com>
2017-06-08 22:34:37 -0400

    adding item to dababase working!

--- app/src/main/java/com/example/zhehui/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/zhehui/inventoryapp/EditorActivity.java
@@ -1,6 +1,7 @@
 package com.example.zhehui.inventoryapp;
 
 import android.app.LoaderManager;
+import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
@@ -8,15 +9,21 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.zhehui.inventoryapp.data.InventoryItemContract;
 import com.example.zhehui.inventoryapp.data.InventoryItemContract.InventoryItemEntry;
 
 public class EditorActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
+
+    public static final String LOG_TAG = EditorActivity.class.getName();
 
     /**
      * Identifier for the item data loader.
@@ -102,6 +109,78 @@ public class EditorActivity extends AppCompatActivity implements
         mNameEditText.setOnTouchListener(mTouchListener);
         mPriceEditText.setOnTouchListener(mTouchListener);
         mQuantityEditText.setOnTouchListener(mTouchListener);
+
+        // Setup the button listener for add & update.
+        Button button = (Button) findViewById(R.id.editor_confirm);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveItem();
+            }
+        });
+    }
+
+    private void saveItem() {
+        // Read from input fields.
+        String nameString = mNameEditText.getText().toString().trim();
+        String priceString = mPriceEditText.getText().toString().trim();
+        String quantityString = mQuantityEditText.getText().toString().trim();
+
+        // Check if this is supposed to be a new item
+        // and check if all the fields in the editor are blank.
+        if (mCurrentItemUri == null &&
+                TextUtils.isEmpty(nameString) &&
+                TextUtils.isEmpty(priceString) &&
+                TextUtils.isEmpty(quantityString)) {
+            Toast.makeText(this, "Fields are empty.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Validate input format, Float for price and Integer for quantity.
+        Float priceFloat;
+        Integer quantityInteger;
+        try {
+            priceFloat = Float.parseFloat(priceString);
+        } catch (NumberFormatException e) {
+            Log.e(LOG_TAG, "Error parsing price value, require 'Float'.");
+            Toast.makeText(this, "Invalid price.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            quantityInteger = Integer.parseInt(quantityString);
+        } catch (NumberFormatException e) {
+            Log.e(LOG_TAG, "Error parsing quantity value, require 'Integer'.");
+            Toast.makeText(this, "Invalid quantity.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Get values.
+        ContentValues values = new ContentValues();
+        values.put(InventoryItemEntry.COLUMN_ITEM_NAME, nameString);
+        values.put(InventoryItemEntry.COLUMN_ITEM_PRICE, priceFloat);
+        values.put(InventoryItemEntry.COLUMN_ITEM_QUANTITY, quantityInteger);
+
+        // Determine adding new item or update existing item.
+        if (mCurrentItemUri == null) {
+            Uri newUri = getContentResolver().insert(InventoryItemEntry.CONTENT_URI, values);
+            if (newUri == null) {
+                Toast.makeText(this, getString(R.string.editor_insert_item_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, getString(R.string.editor_insert_item_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            int rowsAffected = getContentResolver().update(mCurrentItemUri, values, null, null);
+            if (rowsAffected == 0) {
+                Toast.makeText(this, getString(R.string.editor_update_item_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, getString(R.string.editor_update_item_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
